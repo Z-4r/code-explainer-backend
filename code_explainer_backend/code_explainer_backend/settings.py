960ea43a3b8b91ba0452a427ab74cc0dfc32e18b
Z-4r <raunakper777@gmail.com>
2025-07-16 02:50:28 +0530

    whitenoise

--- code_explainer_backend/code_explainer_backend/settings.py
+++ code_explainer_backend/code_explainer_backend/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MIDDLEWARE += ['whitenoice.middleware.WhiteNoiseMiddleware']
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
