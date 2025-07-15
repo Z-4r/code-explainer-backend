8ef3ea0fbc6d6bad4f24fe7bd5956502f851cc89
Z-4r <raunakper777@gmail.com>
2025-07-16 02:47:29 +0530

    render

--- code_explainer_backend/code_explainer_backend/wsgi.py
+++ code_explainer_backend/code_explainer_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_explainer_backed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_explainer_backend.settings')
 
 application = get_wsgi_application()
