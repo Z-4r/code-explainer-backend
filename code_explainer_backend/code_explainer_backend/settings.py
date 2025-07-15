a8b3d80470495fc802f5b7d9e0759f4d3aa0b537
Z-4r <raunakper777@gmail.com>
2025-07-16 02:04:23 +0530

    deploying

--- code_explainer_backend/code_explainer_backend/settings.py
+++ code_explainer_backend/code_explainer_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-=4yp66ej!4tle_lozv!1h34&uwg&w@idyk)k7+((_1ygl46h^)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MIDDLEWARE += ['whitenoice.middleware.WhiteNoiseMiddleware']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
