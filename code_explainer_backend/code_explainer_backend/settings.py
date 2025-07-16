4783d1c771d8753ef336901b490b34584fec2889
Z-4r <raunakper777@gmail.com>
2025-07-17 01:47:25 +0530

    connect frontend to backend

--- code_explainer_backend/code_explainer_backend/settings.py
+++ code_explainer_backend/code_explainer_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apii',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'code_explainer_backend.urls'
@@ -131,3 +133,8 @@ MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://code-explainer-frontend.onrender.com",  # replace with your actual frontend URL
+]
